a8df05dac815985086f83901e4d9c159e6202d1a
danilopvalerio <danilovalerio2003@gmail.com>
2025-06-06 11:05:20 -0300

    WIP: ajustes em andamento

--- Back-end/src/models/Venda.ts
+++ Back-end/src/models/Venda.ts
@@ -1,13 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
 import Funcionario from './Funcionario';
 import ItemVenda from './ItemVenda';
+import Loja from './Loja';
 
 export enum FormaPagamento {
-  DINHEIRO = 'dinheiro',
-  CARTAO_CREDITO = 'cartao_credito',
-  CARTAO_DEBITO = 'cartao_debito',
-  PIX = 'pix',
-  OUTRO = 'outro',
+  DINHEIRO = 'DINHEIRO',
+  CARTAO_CREDITO = 'CARTAO_CREDITO',
+  CARTAO_DEBITO = 'CARTAO_DEBITO',
+  PIX = 'PIX',
+  OUTRO = 'OUTRO',
+}
+
+export enum StatusVenda {
+  COMPLETADA = 'COMPLETADA',
+  CANCELADA = 'CANCELADA',
+  PROCESSANDO = 'PROCESSANDO',
+  ESTORNADA = 'ESTORNADA',
 }
 
 @Entity('venda')
@@ -32,7 +40,7 @@ export default class Venda {
     enum: FormaPagamento,
     nullable: false,
   })
-  forma_pagamento!: FormaPagamento;
+  forma_pagamento!: number;
 
   @Column({ nullable: false })
   id_funcionario!: string;
@@ -43,18 +51,46 @@ export default class Venda {
 
   @OneToMany(() => ItemVenda, (item) => item.venda, { cascade: true })
   itens!: ItemVenda[];
-
+  
+  @Column({ nullable: false })
+  id_loja!: string;
+  
+  @ManyToOne(() => Loja)
+  @JoinColumn({ name: 'id_loja' })
+  loja!: Loja;
+  
+  @Column({
+    type: 'enum',
+    enum: StatusVenda,
+    default: StatusVenda.COMPLETADA,
+  })
+  status!: StatusVenda;
+  
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  criado_em!: Date;
+  
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP'
+  })
+  atualizado_em!: Date;
+  
   constructor(
-    id_funcionario: string,
-    forma_pagamento: FormaPagamento,
-    total: number,
-    desconto: number = 0,
-    acrescimo: number = 0,
+      id_funcionario: string,
+      id_loja: string,
+      forma_pagamento: number,
+      total: number,
+      desconto: number = 0,
+      acrescimo: number = 0,
+      status: StatusVenda = StatusVenda.COMPLETADA
   ) {
     this.id_funcionario = id_funcionario;
+    this.id_loja = id_loja;
     this.forma_pagamento = forma_pagamento;
     this.total = total;
     this.desconto = desconto;
     this.acrescimo = acrescimo;
+    this.status = status;
   }
 }
